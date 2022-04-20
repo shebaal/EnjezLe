f650d7bf2e0d14ad611975933d7712ae8de82118
salman-abdoh <80646924+salman-abdoh@users.noreply.github.com>
2022-04-20 04:26:36 +0300

    create view list_prject and lisworks

---
+++ resources/views/website/listprovidersworks.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>توظيف</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="وظائف,شركات, توظيف, عمل">
+    <meta name="author" content="salman">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <style>
+        .down-arrow-btn {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            -moz-border-radius: 50%;
+            -webkit-border-radius: 50%;
+            -o-border-radius: 50%;
+            background: #219EBC;
+            display: inline-block;
+            margin: 10px auto 20px;
+            color: #fff;
+            font-size: 16px;
+            line-height: 40px;
+            text-align: center;
+        }
+
+        .fa {
+            display: inline-block;
+            font-family: FontAwesome;
+            font-style: normal;
+            font-weight: normal;
+            line-height: 1;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="bd-example">
+            <div class="row  row-cols-3 row-cols-md-2 g-4" style=" direction: rtl;   ">
+                <div class="col-3">
+                    <div class="card">
+                        <img src="21.jpg" alt="">
+
+                        <div class="card-body">
+                            <h3 class="card-title" style=" color: #219EBC;">صممت بيت </h3>
+
+
+                            <p class="card-text mb-auto">اريد ان يصمم لي احد شعر يكون احمر واصغفر واخضر خاص بمنظمة تعمل لحقوق الانسان </p>
+                            <div style="width: 100%;
+              direction: ltr;">
+                                <div class="mb-1 text-muted">محمد سعيد</div>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+           </div>
+        </div>
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    </div>
+</body>
+
+</html>
