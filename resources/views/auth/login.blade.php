e3f1e8c4b8a0a47909172ed2185be1fa32ca9a4c
thorya <thorya.abdulmageed202@gmail.com>
2022-04-24 15:29:24 +0300

    sprint 1authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,62 @@
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-<link rel="stylesheet" href='{{ asset("assests/css/style.css") }}'>
+
+<link rel="stylesheet" href='{{ asset("assests/css/main.css") }}'>
 
 
-    <div class="login_Container">
-            <div class="background w-100">
+ <body>
 
-
-            </div>
-              <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="  col-form-label text-md-end">عنوان بريدك الالكتروني</label>
-
-                            <div class=" ">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+ <div class="loginContainer">
+        <div class="login-box">
+            <div class="login_icon_box">
+                <img src="{{ asset('assests/svg/Layer2.svg') }}" alt="">
+             </div>
+            <h2>تسجيل الدخول</h2>
+            <form method="POST" action="{{ route('login') }}">
+                 @csrf
+                <div class="user-box">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <label>عنوان البريد الالكتروتي</label>
+                       @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="">
-                            <label for="password" class="  col-form-label text-md-end">كلمة السر</label>
-
-                            <div class=" ">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
+                </div>
+                <div class="user-box">
+                    <input type="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <label>كلمة السر</label>
+                        @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="">
-                            <div class="  offset-md-12">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-تذكرني                                     </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="">
-                            <div class=" offset-md-12">
-                                <button type="submit" class="btn btn-primary">
-تسجيل الدخول                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                      هل نسيت كلمة المرور؟
+                </div>
+                <!-- <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span> Submit
+            </a> -->
+    
+   <div class='btn-cont'>
+            <button class='btn' href='#' type="submit">
+                تسجيل
+                  <span class='line-1'></span>
+                  <span class='line-2'></span>
+                  <span class='line-3'></span>
+                  <span class='line-4'></span>
+            </button>
+  </div>
+     </form>
+     <div class="login_links">
+      <div>
+         @if (Route::has('password.request'))
+                                    <a class="btn from-top" href="{{ route('password.request') }}">
+                نسيت كلمة المرور
                                     </a>
                                 @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-           
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+      </div>
+                <a href="{{ route('register') }}"class="btn from-top">إنشاء حساب</a>
+    </div>
+    
+    </div>
+ </body>
