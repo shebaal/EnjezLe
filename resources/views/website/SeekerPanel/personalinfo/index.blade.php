d405f2a56875558e8095c7745458beaced31db99
shebaal <alshamery.sheba@gmail.com>
2022-04-25 13:21:41 +0300

    add some page to panel

--- resources/views/website/SeekerPanel/personalinfo/index.blade.php
+++ resources/views/website/SeekerPanel/personalinfo/index.blade.php
@@ -18,143 +18,76 @@
                       
           </div>
       <div>
-        <h1 class="text-4xl text-white mb-2">أضف عملا </h1>
-        <h1 class="text-4xl text-white mb-4"> ليتم توظيفك</h1>
+        <h1 class="text-4xl text-white mb-2"> أضف مهاره ليتم </h1>
+        <h1 class="text-4xl text-white mb-4">  ايصال الاشعارات لك</h1>
         <div class="banner__cta">
-          <button class="button bg-secondary text-white py-4 px-8">اضافة عمل</button>
+          <button class="button bg-secondary text-white py-4 px-8">اضافة مهاره</button>
         </div>
       </div>
     
     </div>
-    <h2 class="text-2xl mb-5 txt-right">اعمالي</h2>
-    <div class="portos">
-      <div class="flex flex-justify-between flex-align-center bg-white porto">
+    <h2 class="text-2xl mb-5 txt-right">البيانات الشخصية</h2>
+    <div class="myform_body">
       
-        <div class="mt-1">
-          <div class="flex flex-align-center flex-justify-between mb-2">
-            <p class="text-xl mr-4"><b>تصميم شعار لشركة برمجية
-         
-                </p>
-          </div>
+    <form action="">
+    
+      <section class="right bg-white porto">
+        <div class="input-container">
+          <label for="name">الاسم</label>
+          <input type="text" />
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 360 403">
-          <defs>
-            <linearGradient id="linear-gradient" x1="0.174" y1="0.584" x2="0.94" y2="0.263" gradientUnits="objectBoundingBox">
-              <stop offset="0" stop-color="#fff"/>
-              <stop offset="0.507" stop-color="#d7dee0"/>
-              <stop offset="1" stop-color="#114f5e"/>
-            </linearGradient>
-          </defs>
-          <g id="Group_55" data-name="Group 55" transform="translate(-1232 -634)">
-            <g id="Rectangle_101" data-name="Rectangle 101" transform="translate(1232 634)" fill="#11505f" stroke="#707070" stroke-width="1">
-              <rect width="360" height="403" rx="59" stroke="none"/>
-              <rect x="0.5" y="0.5" width="359" height="402" rx="58.5" fill="none"/>
-            </g>
-            <g id="Layer_2" data-name="Layer 2" transform="translate(1301.977 683)">
-              <g id="graphics">
-                <path id="Path_62" data-name="Path 62" d="M40.44,169.17C24.69,160.31,11,144.82,9,128.53c-3.63-29,14-50.82,36.45-60.88,26.84-12,8.93-3.41,111.61-31.94C206.37,22,209.53,9.21,208.42,0c13.33,9.21,17.43,59.41-5.68,74.51C179,90,131.65,93.25,72.16,111.09,19.86,126.77,27.6,155.67,40.44,169.17Z" fill="url(#linear-gradient)"/>
-                <path id="Path_63" data-name="Path 63" d="M29.87,177.69a67.851,67.851,0,0,1,14.78-6.94c74.4-18,80.92-17.62,105.61-24,67.27-17.26,59.19-31.64,60.9-37.38,12.41,14,12.62,57.61-10.38,70.7C172.9,196,123.83,201.18,61.66,217.52c-20.23,5.32-32.07,11.66-40.92,19.17-9.83,8.34-12.52,20.53-5,30.76,6.58,8.94,24.19,7.48,43.7,4.61,34.61-5.09,78.49-15.7,90.81-19.31,55.36-16.19,59.19-31.64,60.9-37.38,12.41,14,12.62,57.61-10.38,70.71-18.22,10.38-35.53,16.35-73,18.73-27.47,1.74-77.37,3.38-101.8-23.42-2.38-2.6-21.39-17.8-25.32-44C-3,213.07,9.63,190.36,29.87,177.69Z" fill="url(#linear-gradient)"/>
-                <path id="Path_64" data-name="Path 64" d="M88.43,160.46h0c-22.28,5.08-43.8,10.29-43.8,10.29l-1.1.38c-1.06-.63-2.1-1.28-3.12-2-12.83-13.5-20.56-42.4,31.73-58.08q5.41-1.62,10.69-3.09C68.19,114.26,47.49,148.19,88.43,160.46Z" fill="url(#linear-gradient)"/>
-                <path id="Path_65" data-name="Path 65" d="M74.7,266.43a38.128,38.128,0,0,0,3.09,2.35c-6.25,1.28-12.43,2.41-18.34,3.28-19.51,2.87-37.12,4.33-43.7-4.61-7.53-10.23-4.84-22.42,5-30.76C37,222.89,58.06,218.23,79.47,213,63.82,220.54,50.69,246,74.7,266.43Z" fill="url(#linear-gradient)"/>
-              </g>
-            </g>
-          </g>
-        </svg>
+        <div class="input-container">
+          <label for="age" required>اللقب</label>
+          <input type="text" />
+        </div>
+        <div class="input-container">
+          <label for="phone"> رقم الهاتف</label>
+          <input type="number" />
+        </div>
+        <div class="input-container">
+          <label for="email">الجنس</label>
+          <input type="text" />
+        </div>
+        <div class="input-container">
+          <label for="email">الدولة</label>
+          <input type="text" />
+        </div>
+       
+      </section>
+      <section class="left bg-white porto">
+        <label class="container-checkbox">  <span class="check_text"> صاحب مشاريع</span>
+  
+          <input type="checkbox" checked="checked">
+          <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">  <span class="check_text"> منجز خدمة  </span>
         
+          <input type="checkbox">
+          <span class="checkmark"></span>
+        </label>
+    
         
-          </div>
-      <div class="flex flex-justify-between flex-align-center bg-white porto">
-        <div class="mt-1">
-            <div class="flex flex-align-center flex-justify-between mb-2">
-              <p class="text-xl mr-4"><b>تصميم شعار لشركة برمجية
-                 
-                  </p>
-                  
-            </div>
-            
-          </div>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 360 403">
-            <defs>
-              <linearGradient id="linear-gradient" x1="0.174" y1="0.584" x2="0.94" y2="0.263" gradientUnits="objectBoundingBox">
-                <stop offset="0" stop-color="#fff"/>
-                <stop offset="0.507" stop-color="#d7dee0"/>
-                <stop offset="1" stop-color="#114f5e"/>
-              </linearGradient>
-            </defs>
-            <g id="Group_55" data-name="Group 55" transform="translate(-1232 -634)">
-              <g id="Rectangle_101" data-name="Rectangle 101" transform="translate(1232 634)" fill="#11505f" stroke="#707070" stroke-width="1">
-                <rect width="360" height="403" rx="59" stroke="none"/>
-                <rect x="0.5" y="0.5" width="359" height="402" rx="58.5" fill="none"/>
-              </g>
-              <g id="Layer_2" data-name="Layer 2" transform="translate(1301.977 683)">
-                <g id="graphics">
-                  <path id="Path_62" data-name="Path 62" d="M40.44,169.17C24.69,160.31,11,144.82,9,128.53c-3.63-29,14-50.82,36.45-60.88,26.84-12,8.93-3.41,111.61-31.94C206.37,22,209.53,9.21,208.42,0c13.33,9.21,17.43,59.41-5.68,74.51C179,90,131.65,93.25,72.16,111.09,19.86,126.77,27.6,155.67,40.44,169.17Z" fill="url(#linear-gradient)"/>
-                  <path id="Path_63" data-name="Path 63" d="M29.87,177.69a67.851,67.851,0,0,1,14.78-6.94c74.4-18,80.92-17.62,105.61-24,67.27-17.26,59.19-31.64,60.9-37.38,12.41,14,12.62,57.61-10.38,70.7C172.9,196,123.83,201.18,61.66,217.52c-20.23,5.32-32.07,11.66-40.92,19.17-9.83,8.34-12.52,20.53-5,30.76,6.58,8.94,24.19,7.48,43.7,4.61,34.61-5.09,78.49-15.7,90.81-19.31,55.36-16.19,59.19-31.64,60.9-37.38,12.41,14,12.62,57.61-10.38,70.71-18.22,10.38-35.53,16.35-73,18.73-27.47,1.74-77.37,3.38-101.8-23.42-2.38-2.6-21.39-17.8-25.32-44C-3,213.07,9.63,190.36,29.87,177.69Z" fill="url(#linear-gradient)"/>
-                  <path id="Path_64" data-name="Path 64" d="M88.43,160.46h0c-22.28,5.08-43.8,10.29-43.8,10.29l-1.1.38c-1.06-.63-2.1-1.28-3.12-2-12.83-13.5-20.56-42.4,31.73-58.08q5.41-1.62,10.69-3.09C68.19,114.26,47.49,148.19,88.43,160.46Z" fill="url(#linear-gradient)"/>
-                  <path id="Path_65" data-name="Path 65" d="M74.7,266.43a38.128,38.128,0,0,0,3.09,2.35c-6.25,1.28-12.43,2.41-18.34,3.28-19.51,2.87-37.12,4.33-43.7-4.61-7.53-10.23-4.84-22.42,5-30.76C37,222.89,58.06,218.23,79.47,213,63.82,220.54,50.69,246,74.7,266.43Z" fill="url(#linear-gradient)"/>
-                </g>
-              </g>
-            </g>
-          </svg>
-          
-     </div>
-      <div class="flex flex-justify-between flex-align-center bg-white porto">
-     
+      
+        <div class="input-container">
+          <label for="address">التخصص</label>
+          <input type="text" />
+        </div>
+        
+        <div class="input-container">
+          <label for="address">المسمى الوظيفي</label>
+          <input type="text" />
+        </div>
 
-        <div class="mt-1">
-            <div class="flex flex-align-center flex-justify-between mb-2">
-              <p class="text-xl mr-4"><b>تصميم شعار لشركة برمجية
-         
-                  </p>
-                  
-            </div>
-          </div>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 360 403">
-            <defs>
-              <linearGradient id="linear-gradient" x1="0.174" y1="0.584" x2="0.94" y2="0.263" gradientUnits="objectBoundingBox">
-                <stop offset="0" stop-color="#fff"/>
-                <stop offset="0.507" stop-color="#d7dee0"/>
-                <stop offset="1" stop-color="#114f5e"/>
-              </linearGradient>
-            </defs>
-            <g id="Group_55" data-name="Group 55" transform="translate(-1232 -634)">
-              <g id="Rectangle_101" data-name="Rectangle 101" transform="translate(1232 634)" fill="#11505f" stroke="#707070" stroke-width="1">
-                <rect width="360" height="403" rx="59" stroke="none"/>
-                <rect x="0.5" y="0.5" width="359" height="402" rx="58.5" fill="none"/>
-              </g>
-              <g id="Layer_2" data-name="Layer 2" transform="translate(1301.977 683)">
-                <g id="graphics">
-                  <path id="Path_62" data-name="Path 62" d="M40.44,169.17C24.69,160.31,11,144.82,9,128.53c-3.63-29,14-50.82,36.45-60.88,26.84-12,8.93-3.41,111.61-31.94C206.37,22,209.53,9.21,208.42,0c13.33,9.21,17.43,59.41-5.68,74.51C179,90,131.65,93.25,72.16,111.09,19.86,126.77,27.6,155.67,40.44,169.17Z" fill="url(#linear-gradient)"/>
-                  <path id="Path_63" data-name="Path 63" d="M29.87,177.69a67.851,67.851,0,0,1,14.78-6.94c74.4-18,80.92-17.62,105.61-24,67.27-17.26,59.19-31.64,60.9-37.38,12.41,14,12.62,57.61-10.38,70.7C172.9,196,123.83,201.18,61.66,217.52c-20.23,5.32-32.07,11.66-40.92,19.17-9.83,8.34-12.52,20.53-5,30.76,6.58,8.94,24.19,7.48,43.7,4.61,34.61-5.09,78.49-15.7,90.81-19.31,55.36-16.19,59.19-31.64,60.9-37.38,12.41,14,12.62,57.61-10.38,70.71-18.22,10.38-35.53,16.35-73,18.73-27.47,1.74-77.37,3.38-101.8-23.42-2.38-2.6-21.39-17.8-25.32-44C-3,213.07,9.63,190.36,29.87,177.69Z" fill="url(#linear-gradient)"/>
-                  <path id="Path_64" data-name="Path 64" d="M88.43,160.46h0c-22.28,5.08-43.8,10.29-43.8,10.29l-1.1.38c-1.06-.63-2.1-1.28-3.12-2-12.83-13.5-20.56-42.4,31.73-58.08q5.41-1.62,10.69-3.09C68.19,114.26,47.49,148.19,88.43,160.46Z" fill="url(#linear-gradient)"/>
-                  <path id="Path_65" data-name="Path 65" d="M74.7,266.43a38.128,38.128,0,0,0,3.09,2.35c-6.25,1.28-12.43,2.41-18.34,3.28-19.51,2.87-37.12,4.33-43.7-4.61-7.53-10.23-4.84-22.42,5-30.76C37,222.89,58.06,218.23,79.47,213,63.82,220.54,50.69,246,74.7,266.43Z" fill="url(#linear-gradient)"/>
-                </g>
-              </g>
-            </g>
-          </svg>
-          
-          
+        <div class="input-container">
+          <label for="comments">النبذه التعريفية</label>
+          <textarea name="comments" id=""></textarea>
+        </div>
+      </section>
+    
+    </form>
     </div>
-    </div>
-    {{-- <div class="charts">
-      <div class="flex flex-justify-between flex-align-center mb-4">
-        <h3 class="text-lg flex flex-align-center">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-2">
-            <path
-              d="M15.4498 3.7803C15.4098 4.0303 15.3898 4.2803 15.3898 4.5303C15.3898 6.7803 17.2098 8.5993 19.4498 8.5993C19.6998 8.5993 19.9398 8.5703 20.1898 8.5303V16.5993C20.1898 19.9903 18.1898 22.0003 14.7898 22.0003H7.40076C3.99976 22.0003 1.99976 19.9903 1.99976 16.5993V9.2003C1.99976 5.8003 3.99976 3.7803 7.40076 3.7803H15.4498ZM15.6508 9.8603C15.3798 9.8303 15.1108 9.9503 14.9498 10.1703L12.5308 13.3003L9.75976 11.1203C9.58976 10.9903 9.38976 10.9393 9.18975 10.9603C8.99076 10.9903 8.81076 11.0993 8.68975 11.2593L5.73076 15.1103L5.66976 15.2003C5.49976 15.5193 5.57976 15.9293 5.87976 16.1503C6.01976 16.2403 6.16976 16.3003 6.33976 16.3003C6.57076 16.3103 6.78976 16.1893 6.92976 16.0003L9.43975 12.7693L12.2898 14.9103L12.3798 14.9693C12.6998 15.1393 13.0998 15.0603 13.3298 14.7593L16.2198 11.0303L16.1798 11.0503C16.3398 10.8303 16.3698 10.5503 16.2598 10.3003C16.1508 10.0503 15.9098 9.8803 15.6508 9.8603ZM19.5899 2C20.9199 2 21.9999 3.08 21.9999 4.41C21.9999 5.74 20.9199 6.82 19.5899 6.82C18.2599 6.82 17.1799 5.74 17.1799 4.41C17.1799 3.08 18.2599 2 19.5899 2Z"
-              fill="#4062FF" />
-          </svg>
-          <span class="mt-2">Total Portfolio</span>
-        </h3>
-        <select value="time_range" class="text-sm">
-          <option name="Weekly" value="0">Weekly</option>
-          <option name="Monthly" value="1">Monthly</option>
-          <option name="Quarterly" value="2">Quarterly</option>
-          <option name="Anually" value="3">Anually</option>
-        </select>
-      </div>
-      <canvas id="myChart"></canvas>
-    </div> --}}
+  </div>
+ 
   </section>
 
 
