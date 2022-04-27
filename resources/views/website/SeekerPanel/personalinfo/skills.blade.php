d153e513203f67b5d98b5f120fece66854ce7136
shebaal <alshamery.sheba@gmail.com>
2022-04-27 03:03:52 +0300

    commit

--- resources/views/website/SeekerPanel/personalinfo/skills.blade.php
+++ resources/views/website/SeekerPanel/personalinfo/skills.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="relative border-none w-full py-4 ">
-          <select name="section" class="text-gray-600 text-right appearance-none py-2.5 border-none bg-gray-100 inline-block rounded leading-tight " style="width: 100%">
+          <select name="section" class="text-gray-600 rtl text-right appearance-none py-2.5 border-none bg-gray-100 inline-block rounded leading-tight " style="width: 100%">
             
             <option class="pt-6" >1</option>
             <option class="pt-6" >1</option>
